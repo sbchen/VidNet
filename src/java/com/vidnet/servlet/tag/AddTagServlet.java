2e8e7ef676c7f61b4d6aaa91ec096b5d4db48e2d
Billy Chen <sbchen@gmail.com>
2012-05-03 16:32:15 -0500

    done

---
+++ src/java/com/vidnet/servlet/tag/AddTagServlet.java
@@ -0,0 +1,117 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vidnet.servlet.tag;
+
+import com.vidnet.db.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author SC
+ */
+public class AddTagServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        int videoid = Integer.parseInt(request.getParameter("vidID"));
+        String dest = "/VideoServlet?videoid=" + videoid;
+        String tag = request.getParameter("addTag");
+        
+        HttpSession session = request.getSession();
+        RequestDispatcher rd;
+        
+        rd = getServletContext().getRequestDispatcher(dest);
+        
+        Tag tempTag;
+        TagModel tagModel = new TagModel();
+        User user;
+        
+        if (session.getAttribute("user") != null) {
+            user = (User)session.getAttribute("user");
+            tempTag = tagModel.addTag(tag, videoid);
+            session.setAttribute("vidTagList", tagModel.getTagList(videoid));
+            rd.forward(request, response);
+        } else {
+            rd.forward(request, response);
+        }
+        
+//        response.setContentType("text/html;charset=UTF-8");
+//        PrintWriter out = response.getWriter();
+//        try {
+//            /*
+//             * TODO output your page here. You may use following sample code.
+//             */
+//            out.println("<html>");
+//            out.println("<head>");
+//            out.println("<title>Servlet AddTagServlet</title>");            
+//            out.println("</head>");
+//            out.println("<body>");
+//            out.println("<h1>Servlet AddTagServlet at " + request.getContextPath() + "</h1>");
+//            out.println("</body>");
+//            out.println("</html>");
+//        } finally {            
+//            out.close();
+//        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
